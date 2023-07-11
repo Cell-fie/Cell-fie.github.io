48d1588271135ecc0abc216b6fe1c5c0c54d627f
Mhjl22 <cellfie1290@gmail.com>
2023-07-11 13:53:59 -0400

    examples

--- app.js
+++ app.js
@@ -21,3 +21,33 @@ document .addEventListener('click', e => {
         dropdown.classList.remove('active')
     })
 })
+
+const drop1 = document.querySelector('#dropdown-links1')
+const dropLinks1 = document.querySelector('.img-1')
+
+drop1.addEventListener('click', function(){
+    print(true)
+    dropLinks1.classList.toggle('active');
+    
+});
+
+const drop2 = document.querySelector('#dropdown-links2')
+const dropLinks2 = document.querySelector('.img-2')
+
+drop2.addEventListener('click', function(){
+    dropLinks2.classList.toggle('active');
+});
+
+const drop3 = document.querySelector('#dropdown-links3')
+const dropLinks3 = document.querySelector('.img-3')
+
+drop3.addEventListener('click', function(){
+    dropLinks3.classList.toggle('active');
+});
+
+const drop4 = document.querySelector('#dropdown-links4')
+const dropLinks4 = document.querySelector('.img-4')
+
+drop4.addEventListener('click', function(){
+    dropLinks4.classList.toggle('active');
+});
