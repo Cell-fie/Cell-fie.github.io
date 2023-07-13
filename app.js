4845adfb8e5216423e04caf8955096576923f792
Mhjl22 <cellfie1290@gmail.com>
2023-07-13 11:33:03 -0400

    IMG update
    added images to examples.html

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
-
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
@@ -9,19 +8,16 @@ menu.addEventListener('click', function(){
 document .addEventListener('click', e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null)return
-    
     let currentDropdown
     if (isDropdownButton) {
         currentDropdown = e.target.closest('[data-dropdown]')
         currentDropdown.classList.toggle('active')
     }
-
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
         if (dropdown === currentDropdown) return
         dropdown.classList.remove('active')
     })
 })
-
 
 // check if document content is loaded
 window.addEventListener("DOMContentLoaded", (event) => {
